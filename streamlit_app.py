be2810d741250875bc3fb07434da954608a767aa
Sameer Goel <samy@Sameers-MacBook-Pro.local>
2023-05-16 00:32:40 +0200

    version1

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,13 @@ import pandas as pd
 from datetime import datetime
 import matplotlib.pyplot as plt
 from matplotlib.table import Table
+from pyairtable import Table as Airtable
 
+api_key = "patVv6UZF6Nu8osdp.264bcf8203bd14bc9752c35998569bc0d19c262914c61f2eeac26006faa631b2"
+base_id = "appxXH2hHvKn4DMLc"
+table_name = "numerologytable"
+
+numerologytable = Airtable(api_key, base_id, table_name)
 
 class NumerologyCalculator:
 
@@ -124,7 +130,7 @@ class LoshuGridPlot:
             '1': "Sun: King",
             '2': "Moon: Queen",
             '3': "Jupiter: Councellor",
-            '4': "Pluto: Myterious Robinhood",
+            '4': "Pluto: Myterious",
             '5': "Mercury: Prince",
             '6': "Venus: Devil's Guru",
             '7': "Neptune: Saint",
@@ -169,12 +175,25 @@ class LoshuGridPlot:
 
 
 def main():
-    st.title("Numerology Calculator")
-    dob = st.date_input("Enter your date of birth", datetime(1990, 1, 28))
-    gender = st.selectbox("Select your gender", ["Male", "Female"])
+    st.title("Loshu Grid Generator")
+    col1, col2 = st.columns(2)
+    with col1:
+        name = st.text_input("Your Full Name")
+        dob = st.date_input("Date of birth", datetime(1990, 1, 28))
+    with col2:
+        gender = st.radio('Sex',["Male", "Female"])
 
     if st.button("Calculate"):
         dob_str = dob.strftime("%d-%m-%Y")
+        day, month, year = (int(part) for part in dob_str.split("-"))
+        psychic_number = NumerologyCalculator.calculate_psychic_number(day)
+        destiny_number = NumerologyCalculator.calculate_destiny_number(dob_str)
+        kua_number = NumerologyCalculator.calculate_kua_number(year, gender.lower())
+
+        col1, col2, col3, col4 = st.columns(4)
+        col2.write(f"Psychic Number: {psychic_number}")
+        col3.write(f"Destiny Number: {destiny_number}")
+        col4.write(f"Kua Number: {kua_number}")
 
         loshu_grid = LoshuGrid(dob_str, gender.lower()).grid
 
@@ -186,6 +205,24 @@ def main():
         LoshuGridPlot.draw_loshu_celestial_grid(loshu_grid, ax=ax)
         st.pyplot(fig)
 
+        # Create string representation of Loshu grid
+        grid_representation = "\n".join([
+            f"| {loshu_grid[4] or '__'} | {loshu_grid[9] or '__'} | {loshu_grid[2] or '__'} |",
+            f"| {loshu_grid[3] or '__'} | {loshu_grid[5] or '__'} | {loshu_grid[7] or '__'} |",
+            f"| {loshu_grid[8] or '__'} | {loshu_grid[1] or '__'} | {loshu_grid[6] or '__'} |"
+        ])
+
+        record = {
+            'Name': name,
+            'DOB': dob.strftime("%Y-%m-%d"),
+            'Gender': gender,
+            'Psychic': psychic_number,
+            'Destiny': destiny_number,
+            'Kua': kua_number,
+            'Chart': grid_representation
+        }
+
+        numerologytable.create(record)
 
 if __name__ == "__main__":
     main()
